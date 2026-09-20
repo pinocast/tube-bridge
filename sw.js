85ed94ebc2211c8570aa7cf6dbe60e1831763cc3
pinocast <6399775+pinocast@users.noreply.github.com>
2026-09-20 16:52:32 +0200

    Refactor service worker to remove caching (not working for android app install)

--- sw.js
+++ sw.js
@@ -1,31 +1,7 @@
-const CACHE_NAME = 'invidious-conv-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'sw.js'
-];
-
-// Installa il Service Worker e salva i file base nella cache locale
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }).then(() => {
-      return self.skipWaiting();
-    })
-  );
+  self.skipWaiting();
 });
 
-// Attiva il Service Worker e pulisce eventuali vecchie cache
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
-// Gestisce le richieste: tenta la rete e, se offline, usa la cache
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
